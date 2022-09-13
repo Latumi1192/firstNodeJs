4f56c8a3bb97da9be3cec333401fce845e2b2c5a
Latumi1192 <honghai.bui@haw-hamburg.de>
2022-09-13 23:25:18 +0200

    Refactor follow Anh's advice

--- src/Components/ErrorReport.js
+++ src/Components/ErrorReport.js
@@ -1,16 +1,17 @@
 import React from 'react';
-import {BrowserRouter as Redirect} from 'react-router-dom';
+import { BrowserRouter as Redirect } from 'react-router-dom';
 
-function ErrorReport(){
-    if(true){
-    return (
-      <div>
+function ErrorReport() {
+
+    const errorPage = (<div>
         <div>Please check your Account and Password</div>
         <div>
-          <button onClick={e => window.location.href = '/'}>Back</button>
+            <button onClick={e => window.location.href = '/'}>Back</button>
         </div>
-      </div>
-    )} else return <Redirect to={'/'} />
-  }
+    </div>);
 
-  export default ErrorReport;
+    return true ? errorPage : <Redirect to={'/'} />;
+
+}
+
+export default ErrorReport;
--- src/Components/LoginForm.js
+++ src/Components/LoginForm.js
@@ -2,53 +2,47 @@ import React from 'react';
 import { BrowserRouter as Redirect } from 'react-router-dom';
 
 
-function LoginForm ()  {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       account: '',
-//       password: '',
-//       thisAccount: 'Honghai',
-//       thisPassword: '123',
-//       isLoggedIn: false,
-//       failedLoggedIn: false
-//     };
+function LoginForm() {
+    //   constructor(props) {
+    //     super(props);
+    //     this.state = {
+    //       account: '',
+    //       password: '',
+    //       thisAccount: 'Honghai',
+    //       thisPassword: '123',
+    //       isLoggedIn: false,
+    //       failedLoggedIn: false
+    //     };
 
-//     this.handleSubmit = this.handleSubmit.bind(this);
-//   }
+    //     this.handleSubmit = this.handleSubmit.bind(this);
+    //   }
 
-  const handleSubmit = () => {
-    if (this.state.account === this.state.thisAccount && this.state.password === this.state.thisPassword) {
-      this.setState({
-        isLoggedIn: true,
-        failedLoggedIn: false
-      });
-    } else {
-      this.setState({ failedLoggedIn: true });
+    const handleSubmit = () => {
+        const validInput = this.state.account === this.state.thisAccount && this.state.password === this.state.thisPassword;
+        validInput ? this.setState({ isLoggedIn: true, failedLoggedIn: false }) : this.setState({ failedLoggedIn: true });
     }
-  }
 
-  
+
+    const loginPage =(<div>
+        <form onSubmit={() => { this.handleSubmit(); }}>
+            <div>
+                <input placeholder='Account' onChange={e => this.setState({ account: e.target.value })}></input>
+            </div>
+            <div>
+                <input placeholder='Password' onChange={e => this.setState({ password: e.target.value })}></input>
+            </div>
+            <div>
+                <button type='submit'>Submit!</button>
+            </div>
+        </form>
+    </div>)
+
     if (!this.state.isLoggedIn && !this.state.failedLoggedIn) {
-      return (
-        <div>
-          <form onSubmit={() => { this.handleSubmit(); }}>
-            <div>
-              <input placeholder='Account' onChange={e => this.setState({ account: e.target.value })}></input>
-            </div>
-            <div>
-              <input placeholder='Password' onChange={e => this.setState({ password: e.target.value })}></input>
-            </div>
-            <div>
-              <button type='submit'>Submit!</button>
-            </div>
-          </form>
-        </div>
-      )
+        return loginPage
     } else if (!this.state.isLoggedIn && this.state.failedLoggedIn) {
-      return <Redirect to={'/error'} />
+        return <Redirect to={'/error'} />
     } else return <Redirect to={'/game'} />
-  
+
 }
 
 export default LoginForm;
